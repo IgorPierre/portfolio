68c229dcdcd3063a6f18fd9f45993f94a82a9d0c
IgorPierre <igor.pierre45@gmail.com>
2024-06-03 14:21:01 -0300

    feat: 💫create the banner component

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Banner = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+
+    h1 {
+        font-family: "Vampiro One", system-ui;
+        font-size: 124px;
+    }
+
+    h2 {
+        font-size: 40px;
+        font-weight: normal;
+    }
+`;
